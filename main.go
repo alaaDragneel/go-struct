ddf14399c1471b581f4772779b5152718bbf6403
alaaDragneel <moaalaa16@gmail.com>
2018-09-19 09:53:41 +0200

    Updating Structs

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
-
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-	jone := person{firstName: "Jone", lastName: "Doe"}
+	// jone := person{"Jone", "Doe"}
 
-	fmt.Println(jone)
+	// jone := person{firstName: "Jone", lastName: "Doe"}
+
+	// var jone person
+
+	// jone.firstName = "Jone"
+	// jone.lastName = "Doe"
+
+	// fmt.Println(jone)
+	// fmt.Printf("%+v", jone)
 }
